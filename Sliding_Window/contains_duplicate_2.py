66a24e6fa426ffa663f41e4966e72361fd568da8
Tejas <tejasburkul967@gmail.com>
2026-04-28 00:06:23 +0530

    problem number 219

---
+++ Sliding_Window/contains_duplicate_2.py
@@ -0,0 +1,15 @@
+class Solution:
+    
+    def containsNearbyDuplicate(self, nums: list[int], k: int) -> bool:
+        seen = set()
+
+        for i in range(len(nums)):
+            if nums[i] in seen:
+                return True
+
+            seen.add(nums[i])
+
+            if len(seen) > k:
+                seen.remove(nums[i - k])
+
+        return False     
