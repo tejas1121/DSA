a293fadd6cb5ecad7174a2e1b192d9b823e5a27f
Tejas <tejasburkul967@gmail.com>
2026-07-30 08:17:56 +0530

    two sum 2

--- Two-Pointer/TwoSum2.py
+++ Two-Pointer/TwoSum2.py
@@ -1,11 +1,13 @@
-i = 0
-j = len(numbers) - 1
-
-while i < j:
-    value = numbers[i] + numbers[j]
-    if target < value:
-        j -= 1
-    elif target > value:
-        i += 1    
-    else:
-        return [i + 1, j + 1]
+class Solution:
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+       l=0
+       r=len(numbers)-1
+       while l<=r:
+         value=numbers[l]+numbers[r]
+         if target>value:
+            l+=1
+         elif target<value:
+            r-=1
+         else:
+            return (l+1,r+1)   
+               
