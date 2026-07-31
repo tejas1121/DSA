2d538d0c9079e4350676d612c253827b47a20d15
Tejas <tejasburkul967@gmail.com>
2026-07-31 17:23:40 +0530

    problem 844

--- Two-Pointer/backspace.py
+++ Two-Pointer/backspace.py
@@ -1,22 +1,46 @@
 class Solution:
     def backspaceCompare(self, s: str, t: str) -> bool:
-       
-        def help(res):
-            st=[]
-            for i in range(len(res)):
-                if res[i]=='#':
-                    if st:
-                      st.pop()
+        i = len(s) - 1
+        j = len(t) - 1
+
+        while i >= 0 or j >= 0:
+
+            skip = 0
+
+            # Find next valid character in s
+            while i >= 0:
+                if s[i] == '#':
+                    skip += 1
+                    i -= 1
+                elif skip > 0:
+                    skip -= 1
+                    i -= 1
                 else:
-                    st.append(res[i])
-            return st
-        res1=help(s)
-        res2=help(t)
-        if len(res1)!=len(res2):
-            return False
-        for i in range(len(res1)):
-            if res1[i]!=res2[i]:
+                    break
+
+            skip = 0
+
+            # Find next valid character in t
+            while j >= 0:
+                if t[j] == '#':
+                    skip += 1
+                    j -= 1
+                elif skip > 0:
+                    skip -= 1
+                    j -= 1
+                else:
+                    break
+
+            # Both point to valid characters
+            if i >= 0 and j >= 0:
+                if s[i] != t[j]:
+                    return False
+
+            # One string finished before the other
+            elif i >= 0 or j >= 0:
                 return False
-        return True   
 
+            i -= 1
+            j -= 1
 
+        return True
