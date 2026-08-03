9f4d523b8f2b567d03cfea2aac7a41b8c2810e47
Tejas <tejasburkul967@gmail.com>
2026-08-03 08:50:24 +0530

    problem number 347

--- Hashing/k_most_frequent.py
+++ Hashing/k_most_frequent.py
@@ -6,4 +6,18 @@ class Solution:
         for i in range(k):
             ans.append(sorted_freq[i][0])
         return ans    
+
+#By frequency sort
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        freq={}
+        for i in nums:
+            freq[i]=freq.get(i,0)+1
+        sort_freq=sorted(freq.items(),key=lambda x : x[1],reverse=True)
+        ans=[]
+        for i in range(k):
+            ans.append(sort_freq[i][0])
+        return ans    
+            
+
         
