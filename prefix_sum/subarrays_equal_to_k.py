e6ae4fe3153135da9c8d299f7470f7c8638aaee2
Tejas <tejasburkul967@gmail.com>
2026-08-04 12:13:55 +0530

    problem number 560

--- prefix_sum/subarrays_equal_to_k.py
+++ prefix_sum/subarrays_equal_to_k.py
@@ -1,18 +1,17 @@
 class Solution:
     def subarraySum(self, nums: List[int], k: int) -> int:
         n=len(nums)
-        prefix=[0]*n
-        prefix[0]=nums[0]
-        for i in range(1,n):
-            prefix[i]=prefix[i-1]+nums[i]
+        prefix=0
+        
         mp={0:1}
         count=0
         for i in range(n):
-            c=prefix[i]-k
+            prefix+=nums[i]
+            c=prefix-k
             if c in mp:
                 count+=mp[c]
             
-            mp[prefix[i]]=mp.get(prefix[i],0)+1
+            mp[prefix]=mp.get(prefix,0)+1
         return count                
 
 
