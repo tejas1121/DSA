379d88dde219373069c2a6018cd350a5dce09f6f
Tejas <tejasburkul967@gmail.com>
2026-08-15 23:47:42 +0530

    problem 876

--- linked_list/mid_node.py
+++ linked_list/mid_node.py
@@ -16,5 +16,15 @@ class Solution:
         for i in range(mid):
             mid_node=mid_node.next
         return mid_node    
+#slow and fast pointer approach
+class Solution:
+    def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        slow = head
+        fast = head
 
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+
+        return slow
         
