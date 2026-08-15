e363098c1414bd35f0d8a2cb9d1ac97b2fb9fb6f
Tejas <tejasburkul967@gmail.com>
2026-08-15 23:45:54 +0530

    problem 876

---
+++ linked_list/mid_node.py
@@ -0,0 +1,20 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        curr=head
+
+        count=0
+        while curr:
+            curr=curr.next
+            count+=1
+        mid=count//2
+        mid_node=head
+        for i in range(mid):
+            mid_node=mid_node.next
+        return mid_node    
+
+        
