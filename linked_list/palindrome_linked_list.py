4b3c2b1efce73625d0fa8bb29b20bb2be948b8d1
Tejas <tejasburkul967@gmail.com>
2026-08-17 10:26:28 +0530

    problem 234

---
+++ linked_list/palindrome_linked_list.py
@@ -0,0 +1,18 @@
+class Solution:
+    def isPalindrome(self, head: Optional[ListNode]) -> bool:
+        stack = []
+
+        curr = head
+
+        while curr:
+            stack.append(curr.val)
+            curr = curr.next
+
+        curr = head
+
+        while curr:
+            if curr.val != stack.pop():
+                return False
+            curr = curr.next
+
+        return True
