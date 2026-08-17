336f1b03986fa058df89cfe6140eea60fa69af9c
Tejas <tejasburkul967@gmail.com>
2026-08-17 10:31:02 +0530

    problem 234

--- linked_list/palindrome_linked_list.py
+++ linked_list/palindrome_linked_list.py
@@ -16,3 +16,36 @@ class Solution:
             curr = curr.next
 
         return True
+#two pointer approach
+class Solution:
+    def isPalindrome(self, head: Optional[ListNode]) -> bool:
+
+        # Find middle
+        slow = head
+        fast = head
+
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+
+        # Reverse second half
+        prev = None
+
+        while slow:
+            next_node = slow.next
+            slow.next = prev
+            prev = slow
+            slow = next_node
+
+        # Compare
+        left = head
+        right = prev
+
+        while right:
+            if left.val != right.val:
+                return False
+
+            left = left.next
+            right = right.next
+
+        return True    
